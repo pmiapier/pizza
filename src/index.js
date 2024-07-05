b09e541a6001a32bbdeda0f7a4c089d1ea534e98
pmiapier <pmiapier@proton.me>
2024-07-05 18:54:03 +0700

    added fragment and classes for SOLD OUT Product,using text conditoinally

--- src/index.js
+++ src/index.js
@@ -78,13 +78,20 @@ function Menu() {
       <h2>Our Menu </h2>
 
       {numPizzas > 0 ? (
-        <ul className='pizzas'>
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisin. Six creative dishes to choose from. All
+            from our stone oven, all organic and delicious.{' '}
+          </p>
+
+          <ul className='pizzas'>
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
-        <p>We are still working on our menu. Please come back later</p>
+        <p>We are still working on our menu. Please come back later.</p>
       )}
 
       {/* <Pizza
@@ -106,15 +113,22 @@ function Menu() {
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
 
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className='pizza'>
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''} `}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>$CAD {pizzaObj.price}</span>
+
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+
+        <span> {pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
     </li>
   );
